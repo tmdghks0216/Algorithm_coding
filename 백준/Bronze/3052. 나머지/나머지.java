6ed01caeb472052ee3e23cd385c1e918bfec3f22
tmdghks0216 <69497291+tmdghks0216@users.noreply.github.com>
2025-03-14 17:53:41 +0900

    [Bronze II] Title: 나머지, Time: 220 ms, Memory: 19892 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt() % 42 ;
+        }
+
+      System.out.println(Arrays.stream(arr).distinct().count());           
+    }
+}
