7f6fdcf4143daa48b957382b25f9b3f0eb3e1064
tmdghks0216 <69497291+tmdghks0216@users.noreply.github.com>
2025-03-15 01:01:50 +0900

    [Bronze V] Title: 아스키 코드, Time: 88 ms, Memory: 12944 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] arg){
+        Scanner sc =new Scanner(System.in);
+        String T = sc.next(); 
+        System.out.print((int) T.charAt(0));
+    }
+}
